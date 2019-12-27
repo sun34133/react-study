c89f3949f783c91b327e7bf0cc071b8ccb64bde6
Sunil Kumar <sun34133@gmail.com>
2019-12-27 19:08:17 +0530

    reference with readct hooks
    reference with readct hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 import classes from "./Cockpit.css";
 
 const Cockpit = props => {
+  const toggleBtnRef = useRef(null);
+
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
 
     // Http request...
-    setTimeout(() => {
-      alert("Saved data to cloud!!");
-    }, 1000);
+    // setTimeout(() => {
+    //   alert("Saved data to cloud!!");
+    // }, 1000);
+    toggleBtnRef.current.click();
 
     return () => {
       console.log("[Cockpit.js] cleanup work in useEffect!");
@@ -43,9 +46,9 @@ const Cockpit = props => {
       <p className={assignedClasses.join(" ")}>
         This is test for the child paragraph.
       </p>
-      <button 
-        className={btnClass} 
-        onClick={props.clicked}>Toggle Students</button>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
+        Toggle Students
+      </button>
     </div>
   );
 };
