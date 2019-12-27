4c9553eac0287465bf9801323c5074271e3a317b
Sunil Kumar <sun34133@gmail.com>
2019-12-27 16:13:21 +0530

    aux component
    aux component

--- src/components/Students/Student/Student.js
+++ src/components/Students/Student/Student.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 
+import Aux from "../../../hoc/Aux";
 import classes from "./Student.css";
 
 class Student extends Component {
   render() {
     console.log("[Student.js] rendering....");
     return (
-      <div className={classes.Student}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.updated} value={this.props.name} />
-      </div>
+        <input
+          type="text"
+          onChange={this.props.updated}
+          value={this.props.name}
+        />
+      </Aux>
     );
   }
 }
